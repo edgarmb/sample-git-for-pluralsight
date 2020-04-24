e0a669a0ced68732958c2789e5bcdb3bb173ce01
Edgar <emagallanes016@gmail.com>
2020-04-24 11:54:32 -0500

    created Main class

---
+++ src/training/Main.java
@@ -0,0 +1,17 @@
+package training;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 50;
+		int b = 2;
+		int c = a*b;
+		int d = a-b;
+		
+		System.out.print(c);
+		System.out.print("\n");
+		System.out.print(d);
+		
+		}
+
+}
